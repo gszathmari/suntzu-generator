faa46c7dff38c0ef24bd3c45e20a08207d00ecda
Gabor Szathmari <gszathmari@gmail.com>
2015-03-27 22:55:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# suntzu-generator
+
+This application generates random made up quotes from Sun Tzu's Art of War
---
+++ app/generate.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import markovgen
+import time
+import json
+
+def getQuote(corpus=None):
+  # Recursion goodness
+  if corpus:
+    quoteGenerator = corpus
+  else:
+    quoteGenerator = markovgen.Markov()
+    lines          = [line.strip() for line in open('../data/artofwar.txt')]
+    [quoteGenerator.feed(line) for line in lines]
+
+  # Generate quote
+  quote	= quoteGenerator.generate_markov_text(max_size=128, seed=None, backward=False).capitalize()
+
+  # If quote is too short, roll again
+  if len(quote) > 128 or len(quote) < 50:
+    return getQuote(quoteGenerator)
+  else:
+    return str(quote)
+
+# Generate JSON output
+def generateResponse():
+  response = {'quote': getQuote(),
+              'timestamp': int(time.time())}
+  return json.dumps(response)
+
+# Simple HTTP server
+def run(environ, start_response):
+  requestPath = str(environ['PATH_INFO'])
+
+  if requestPath == '/v1/getquote': 
+    data = generateResponse().encode('utf-8')
+    status = '200 OK'
+    response_headers = [
+      ('Content-type','application/json'),
+      ('Content-Length', str(len(data)))
+    ]
+  else:
+    data = 'Not found'.encode('utf-8')
+    status = '404 Not Found'
+    response_headers = [
+      ('Content-type','text/plain'),
+      ('Content-Length', 0)
+    ]
+
+  start_response(status, response_headers)
+  return iter([data])
